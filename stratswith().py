5889420cca3407a46f5372074b6b0a412fe0029b
Deepan David <51745197+DeepanD2@users.noreply.github.com>
2020-07-22 15:45:27 +0530

    Create stratswith().py

---
+++ stratswith().py
@@ -0,0 +1,24 @@
+The first line of input contains the original string. The next line contains the substring.
+
+Constraints
+
+
+Each character in the string is an ascii character.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input
+
+ABCDCDC
+CDC
+Sample Output
+2
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
